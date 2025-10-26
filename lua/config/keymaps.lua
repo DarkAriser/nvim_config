e5224c625b58fe2949218cd55bf8e2542e0cdc90
Burton Copeland <bcope@Burtons-MacBook-Air.local>
2025-10-26 10:55:47 -0500

    Added multicursor

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -24,27 +24,73 @@ map("n", ",dd", '"+dd', { noremap=true, desc = "Delete line and copy" })
 map("n", ",D",  '"+D',  { noremap=true, desc = "Delete remaining line and copy" })
 
 -- Pounce
-map("n", "s",  function() require("pounce").pounce({}) end)
-map("n", "S",  function() require("pounce").pounce({ do_repeat = true }) end)
-map("x", "s",  function() require("pounce").pounce({}) end)
-map("o", "gs", function() require("pounce").pounce({}) end)
+local pounce = require("pounce").pounce
+map("n", "s",  function() pounce({}) end)
+map("n", "S",  function() pounce({ do_repeat = true }) end)
+map("x", "s",  function() pounce({}) end)
+map("o", "gs", function() pounce({}) end)
 
 -- Dial
-map("n", "<C-a>",  function() require("dial.map").manipulate("increment", "normal")  end)
-map("n", "<C-x>",  function() require("dial.map").manipulate("decrement", "normal")  end)
-map("n", "<C-ga>", function() require("dial.map").manipulate("increment", "gnormal") end)
-map("n", "<C-gx>", function() require("dial.map").manipulate("decrement", "gnormal") end)
-map("v", "<C-a>",  function() require("dial.map").manipulate("increment", "visual")  end)
-map("v", "<C-x>",  function() require("dial.map").manipulate("decrement", "visual")  end)
-map("v", "<C-ga>", function() require("dial.map").manipulate("increment", "gvisual") end)
-map("v", "<C-gx>", function() require("dial.map").manipulate("decrement", "gvisual") end)
+local dialmanip = require("dial.map").manipulate
+map("n", "<C-a>",  function() dialmanip("increment", "normal")  end)
+map("n", "<C-x>",  function() dialmanip("decrement", "normal")  end)
+map("n", "<C-ga>", function() dialmanip("increment", "gnormal") end)
+map("n", "<C-gx>", function() dialmanip("decrement", "gnormal") end)
+map("v", "<C-a>",  function() dialmanip("increment", "visual")  end)
+map("v", "<C-x>",  function() dialmanip("decrement", "visual")  end)
+map("v", "<C-ga>", function() dialmanip("increment", "gvisual") end)
+map("v", "<C-gx>", function() dialmanip("decrement", "gvisual") end)
 
 -- vim-easy-align
 -- map("v", "ga", "<Plug>(EasyAlign)")
 -- map("n", "ga", "<Plug>(EasyAlign)")
 
 -- nvim-scissors
-map( "n", "<leader>se",          function() require("scissors").editSnippet()   end, { desc = "Snippet: Edit" })
-map( { "n", "x" }, "<leader>sa", function() require("scissors").addNewSnippet() end, { desc = "Snippet: Add" }
-)
+local scis = require("scissors")
+map( "n", "<leader>se",          function() scis.editSnippet()   end, { desc = "Snippet: Edit" })
+map( { "n", "x" }, "<leader>sa", function() scis.addNewSnippet() end, { desc = "Snippet: Add" })
+
+-- multi-curosr
+-- Add or skip cursor above/below the main cursor.
+local mc = require("multicursor-nvim")
+map({"n", "x"}, "<up>",           function() mc.lineAddCursor(-1)  end)
+map({"n", "x"}, "<down>",         function() mc.lineAddCursor(1)   end)
+map({"n", "x"}, "<leader><up>",   function() mc.lineSkipCursor(-1) end)
+map({"n", "x"}, "<leader><down>", function() mc.lineSkipCursor(1)  end)
+
+-- Add or skip adding a new cursor by matching word/selection
+map({"n", "x"}, "<leader>n", function() mc.matchAddCursor(1)   end)
+map({"n", "x"}, "<leader>s", function() mc.matchSkipCursor(1)  end)
+map({"n", "x"}, "<leader>N", function() mc.matchAddCursor(-1)  end)
+map({"n", "x"}, "<leader>S", function() mc.matchSkipCursor(-1) end)
+
+-- Add and remove cursors with control + left click.
+map("n", "<c-leftmouse>", mc.handleMouse)
+map("n", "<c-leftdrag>", mc.handleMouseDrag)
+map("n", "<c-leftrelease>", mc.handleMouseRelease)
+
+-- Disable and enable cursors.
+map({"n", "x"}, "<c-q>", mc.toggleCursor)
+
+-- Mappings defined in a keymap layer only apply when there are
+-- multiple cursors. This lets you have overlapping mappings.
+mc.addKeymapLayer(function(layerSet)
+
+  -- Select a different cursor as the main one.
+  layerSet({"n", "x"}, "<left>", mc.prevCursor)
+  layerSet({"n", "x"}, "<right>", mc.nextCursor)
+
+  -- Delete the main cursor.
+  layerSet({"n", "x"}, "<leader>x", mc.deleteCursor)
+
+  -- Enable and clear cursors using escape.
+  layerSet("n", "<esc>", function()
+    if not mc.cursorsEnabled() then
+      mc.enableCursors()
+    else
+      mc.clearCursors()
+    end
+  end)
+end)
+
 
